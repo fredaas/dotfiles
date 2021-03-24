d3e714c6d7bc16894e459e5a8ef2a45ca3d522b1
Fredrik Ås <fredaas86@gmail.com>
2021-03-24 14:24:40 +0100

    Implement support for selecting blocks

--- sublime/plugins/pyselect/pyselect.py
+++ sublime/plugins/pyselect/pyselect.py
@@ -1,12 +1,18 @@
 import sublime
 import sublime_plugin
 
+from Default.paragraph import expand_to_paragraph
+
+class SelectBlock(sublime_plugin.TextCommand):
+    def run(self, edit):
+        view = self.view
+        region = view.sel()[0]
+        region = expand_to_paragraph(view, region.begin())
+        sublime.Selection.add(view, sublime.Region(region.end(), region.begin()))
 
 class SelectLine(sublime_plugin.TextCommand):
     def run(self, edit):
         view = self.view
-        regions = view.sel()
-        if len(regions) > 1:
-            return
-        region = view.line(regions[0])
+        region = view.sel()[0]
+        region = view.line(region)
         sublime.Selection.add(view, sublime.Region(region.begin(), region.end()))
